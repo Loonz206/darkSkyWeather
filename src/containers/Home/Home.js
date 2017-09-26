3f444f6eae7d2ecde3a07135e49db29a789a4ee7
Lenny Peters <lpeters@hollandamericagroup.com>
2017-09-26 10:59:13 -0700

    Master - figure out why location isn't being handled

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Weather from '../../components/Weather/Weather';
 
 class Home extends Component {
     render () {
         return (
             <div className="home-container">
-
+                <Weather />
             </div>
         )
     }
