452dbab428bdf93cbd39683a2f02a85e7fd3e3f0
Lenny Peters <mr.lennypeters@gmail.com>
2017-10-06 15:15:44 -0700

    Master - changes to the UI

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -3,15 +3,36 @@ import React, { Component} from 'react';
 class Weather extends Component {
     constructor(){
         super()
-        this.state = {}
+        this.state = {
+            summary: "",
+            forecast:  [],
+            currently: {},
+            loading: true
+        };
         this.getWeather = this.getWeather.bind(this);
     }
-    getWeather(){
-        //
+    getWeather() {
+        const url = 'api/weather/47.6227156713256,-122.360553979498';
+
+        return fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('rest call returned');
+                return this.setState({
+                    summary: data.daily.summary,
+                    forecast: data.daily.data,
+                    currently: data.currently,
+                    loading: false
+                });
+            })
+            .catch((error) => console.log(error));
+    }
+    componentDidMount() {
+        this.getWeather();
     }
     render (){
         return (
-            <div></div>
+            <div>{this.state}</div>
         )
     }
 }
